82f830264399e112dd06943adf8b48ba463b6785
HaveChen <1065966570@qq.com>
2018-12-10 15:50:50 +0800

    代码提交

---
+++ src/com/cy/a_1jdk/JDK_Related.java
@@ -0,0 +1,12 @@
+package com.cy.a_1jdk;
+
+/**
+ * 
+ * JDK安装配置
+ * 
+ * @autho chenyou
+ * @time 2018年12月10日 下午3:45:44
+ */
+public class JDK_Related {
+
+}
---
+++ src/com/cy/a_2datatype/DataType.java
@@ -0,0 +1,12 @@
+package com.cy.a_2datatype;
+
+/**
+ * 
+ * 基本数据类型及注意
+ * 
+ * @autho chenyou
+ * @time 2018年12月10日 下午3:46:56
+ */
+public class DataType {
+
+}
